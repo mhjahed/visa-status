9d0ad0c4bcab27580e428609ab4a18be63f52189
INVISIBLE- MH <146859747+mhjahed@users.noreply.github.com>
2026-06-06 23:35:33 +0600

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@
     // 🔧 DEVELOPER: CHANGE THESE AS NEEDED
     // ============================================
     const SITE_VERSION = '2.4.9';
-    const LAST_UPDATE = "2026-05-23T22:30:00";   // Format: YYYY-MM-DDTHH:MM:SS
-    const NEXT_UPDATE = "2026-05-27T22:00:00";   // Must be FUTURE date!
+    const LAST_UPDATE = "2026-06-6T22:30:00";   // Format: YYYY-MM-DDTHH:MM:SS
+    const NEXT_UPDATE = "2026-06-12T22:00:00";   // Must be FUTURE date!
     // ============================================
 
     const VERSION_KEY = 'visa_status_version';
